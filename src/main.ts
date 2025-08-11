1041d0d1c6d83c4eddf414f6f2d9f50fb239c637
Roberto <rmga208@gmail.com>
2025-08-11 01:11:16 -0400

    tengo suenno-v2

--- src/main.ts
+++ src/main.ts
@@ -6,69 +6,32 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix("api");
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  
+
+// Aumentar límites de payload
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  
+
   app.enableCors({
-    origin: process.env.ORIGIN_CORS || "*",
+    origin: process.env.ORIGIN_CORS || "http://localhost:3000",
   });
-  
+
   const config = new DocumentBuilder()
     .setTitle('API documentation')
+    // .setDescription('La descripción de la API')
     .setVersion('1.0')
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
-  
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+
+  await app.listen(process.env.PORT || 8000);
 }
-
 bootstrap();
-
-// import { NestFactory } from "@nestjs/core";
-// import { AppModule } from "./app.module";
-// import { ValidationPipe } from "@nestjs/common";
-// import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-// import { json, urlencoded } from 'express';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.setGlobalPrefix("api");
-
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//     }),
-//   );
-
-// // Aumentar límites de payload
-//   app.use(json({ limit: '50mb' }));
-//   app.use(urlencoded({ extended: true, limit: '50mb' }));
-
-//   app.enableCors({
-//     origin: process.env.ORIGIN_CORS || "http://localhost:3000",
-//   });
-
-//   const config = new DocumentBuilder()
-//     .setTitle('API documentation')
-//     // .setDescription('La descripción de la API')
-//     .setVersion('1.0')
-//     .build();
-    
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup("docs", app, document);
-
-//   await app.listen(process.env.PORT || 8000);
-// }
-// bootstrap();
